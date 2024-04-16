ddffacf12ce29535105c2e2a6fa50e6014d071ce
Maciej Borkowski <mmborkowski11@gmail.com>
2024-04-16 15:18:17 +0200

    feat: repl

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+    "fmt"
+    "os"
+    "os/user"
+    "interpreter/repl"
+)
 
+func main() {
+    user, err := user.Current()
+    if err != nil {
+        panic(err)
+    }
+    fmt.Printf("Hello %s!\n", user.Username)
+    repl.Start(os.Stdin, os.Stdout)
 }
