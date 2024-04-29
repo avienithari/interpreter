8d39564f267eebbcebd9656ddfcbb01f3e0f8271
avienithari <125766902+avienithari@users.noreply.github.com>
2024-04-29 20:26:24 +0200

    feat: parsing array literals

--- parser/parser.go
+++ parser/parser.go
@@ -60,6 +60,7 @@ func New(l *lexer.Lexer) *Parser {
     p.registerPrefix(token.BANG, p.parsePrefixExpression)
     p.registerPrefix(token.MINUS, p.parsePrefixExpression)
     p.registerPrefix(token.LPAREN, p.parseGroupedExpression)
+    p.registerPrefix(token.LBRACKET, p.parseArrayLiteral)
 
     p.registerPrefix(token.IF, p.parseIfExpression)
     p.registerPrefix(token.FUNCTION, p.parseFunctionLiteral)
@@ -369,32 +370,8 @@ func (p *Parser) parseInfixExpression(left ast.Expression) ast.Expression {
 
 func (p *Parser) parseCallExpression(function ast.Expression) ast.Expression {
     exp := &ast.CallExpression{Token: p.curToken, Function: function}
-    exp.Arguments = p.parseCallArguments()
+    exp.Arguments = p.parseExpressionList(token.RPAREN)
     return exp
-}
-
-func (p *Parser) parseCallArguments() []ast.Expression {
-    args := []ast.Expression{}
-
-    if p.peekTokenIs(token.RPAREN) {
-        p.nextToken()
-        return args
-    }
-
-    p.nextToken()
-    args = append(args, p.parseExpression(LOWEST))
-
-    for p.peekTokenIs(token.COMMA) {
-        p.nextToken()
-        p.nextToken()
-        args = append(args, p.parseExpression(LOWEST))
-    }
-
-    if !p.expectPeek(token.RPAREN) {
-        return nil
-    }
-
-    return args
 }
 
 func (p *Parser) peekPrecedence() int {
@@ -432,4 +409,36 @@ func (p *Parser) parseBlockStatement() *ast.BlockStatement {
     }
 
     return block
+}
+
+func (p *Parser) parseArrayLiteral() ast.Expression {
+    array := &ast.ArrayLiteral{Token: p.curToken}
+
+    array.Elements = p.parseExpressionList(token.RBRACKET)
+
+    return array
+}
+
+func (p *Parser) parseExpressionList(end token.TokenType) []ast.Expression {
+    list := []ast.Expression{}
+
+    if p.peekTokenIs(end) {
+        p.nextToken()
+        return list
+    }
+
+    p.nextToken()
+    list = append(list, p.parseExpression(LOWEST))
+
+    for p.peekTokenIs(token.COMMA) {
+        p.nextToken()
+        p.nextToken()
+        list = append(list, p.parseExpression(LOWEST))
+    }
+
+    if !p.expectPeek(end) {
+        return nil
+    }
+
+    return list
 }
