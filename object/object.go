9129877e2323f4f67f8402e0865b5768952731c8
Maciej Borkowski <mmborkowski11@gmail.com>
2024-05-02 10:44:26 +0200

    feat: quote macro

--- object/object.go
+++ object/object.go
@@ -23,6 +23,8 @@ const (
 
     ARRAY_OBJ = "ARRAY"
     HASH_OBJ = "HASH"
+
+    QUOTE_OBJ = "QUOTE"
 )
 
 type ObjectType string
@@ -182,4 +184,13 @@ func (h *Hash) Inspect() string {
 
 type Hashable interface {
     HashKey() HashKey
+}
+
+type Quote struct {
+    Node ast.Node
+}
+
+func (q *Quote) Type() ObjectType { return QUOTE_OBJ }
+func (q *Quote) Inspect() string {
+    return "QUOTE(" + q.Node.String() + ")"
 }
