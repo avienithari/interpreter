90f520b3f738681121256836fc780ba7cd47e697
avienithari <125766902+avienithari@users.noreply.github.com>
2024-05-03 13:40:36 +0200

    feat: parsing macros

--- parser/parser.go
+++ parser/parser.go
@@ -67,6 +67,7 @@ func New(l *lexer.Lexer) *Parser {
 
     p.registerPrefix(token.IF, p.parseIfExpression)
     p.registerPrefix(token.FUNCTION, p.parseFunctionLiteral)
+    p.registerPrefix(token.MACRO, p.parseMacroLiteral)
 
     p.registerPrefix(token.TRUE, p.parseBoolean)
     p.registerPrefix(token.FALSE, p.parseBoolean)
@@ -488,4 +489,22 @@ func (p *Parser) parseHashLiteral() ast.Expression {
     }
 
     return hash
+}
+
+func (p *Parser) parseMacroLiteral() ast.Expression {
+    lit := &ast.MacroLiteral{Token: p.curToken}
+
+    if !p.expectPeek(token.LPAREN) {
+        return nil
+    }
+
+    lit.Parameters = p.parseFunctionParameters()
+
+    if !p.expectPeek(token.LBRACE) {
+        return nil
+    }
+
+    lit.Body = p.parseBlockStatement()
+
+    return lit
 }
